cc10906604ce52fb511e34466458a1126cdf0b28
se7ensde <58447233+se7ensde@users.noreply.github.com>
2020-04-11 19:03:59 -0700

    think this is the issue

--- common.mk
+++ common.mk
@@ -21,9 +21,8 @@ DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
     $(LOCAL_PATH)/overlay-bliss
 
-PRODUCT_ENFORCE_RRO_TARGETS := *
-PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
-    $(LOCAL_PATH)/overlay-bliss
+PRODUCT_ENFORCE_RRO_TARGETS := \
+    framework-res
 
 # A/B
 AB_OTA_UPDATER := true
